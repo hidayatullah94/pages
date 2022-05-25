2a8640892900777e5eabb2e32046d745556d2ed7
hidayatullah94 <hidayatullah1780@gmail.com>
2022-05-25 09:07:20 +0700

    create pages

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+
+<h1>Welcome to Svelte</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, consequatur. Doloribus, inventore expedita! Quae, quis at. Corrupti ab explicabo repellat! Cumque ratione iusto exercitationem reiciendis expedita enim odit cum necessitatibus rerum quidem, labore atque accusamus nihil consequatur quasi quod illum magnam odio consectetur omnis debitis accusantium sint. Sunt, voluptatibus atque?</p>
+
+<img src="./av.jfif" alt="ava">
+
+<style>
+	h1{
+		text-align: center;
+	}
+	p{
+		font-style: italic;
+	}
+	img{
+		width: 80vw;
+		height: auto;
+	}
+
+</style>
